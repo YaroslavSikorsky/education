dc6da2fe8cb328caf395566bde38c1181ac4b66b
Yaroslav Sikorsky <yrl.sikorsky@gmail.com>
2023-07-10 20:49:28 +0300

    Tasks added

---
+++ src/javarush/tasks/JavaCore/tasks05/task1505/Solution.java
@@ -0,0 +1,32 @@
+package javarush.tasks.JavaCore.tasks05.task1505;
+
+/* 
+ООП - исправь ошибки в наследовании
+*/
+
+public class Solution {
+    public interface HasWeight {
+        int getWeight();
+    }
+
+    public interface HasHeight {
+        int getHeight();
+    }
+
+    public static class Human implements HasWeight, HasHeight {
+        @Override
+        public int getWeight() {
+            return 1;
+        }
+         @Override
+        public int getHeight() {
+            return 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human.getWeight());
+        System.out.println(human.getHeight());
+    }
+}
---
+++ src/javarush/tasks/JavaCore/tasks05/task1513/Solution.java
@@ -0,0 +1,22 @@
+package javarush.tasks.JavaCore.tasks05.task1513;
+
+/* 
+Максимально простой код-3
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public interface Runnable {
+
+    }
+    
+    public class Machine implements Runnable {
+        
+    }
+
+    public class Car extends Machine  {
+       
+    }
+}
