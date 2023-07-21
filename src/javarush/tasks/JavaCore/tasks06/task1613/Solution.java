3b0d94f1b0fb9845c33c00955d29f88d2ea4bb0c
Yaroslav Sikorsky <yrl.sikorsky@gmail.com>
2023-07-21 18:55:35 +0300

    Tasks added

---
+++ src/javarush/tasks/JavaCore/tasks06/task1613/Solution.java
@@ -0,0 +1,55 @@
+package javarush.tasks.JavaCore.tasks06.task1613;
+
+/* 
+Big Ben clock
+*/
+
+public class Solution {
+    public static volatile boolean isStopped = false;
+
+    public static void main(String[] args) throws InterruptedException {
+        Clock clock = new Clock("Лондон", 23, 59, 57);
+        Thread.sleep(4000);
+        isStopped = true;
+        Thread.sleep(1000);
+    }
+
+    public static class Clock extends Thread {
+        private String cityName;
+        private int hours;
+        private int minutes;
+        private int seconds;
+
+        public Clock(String cityName, int hours, int minutes, int seconds) {
+            this.cityName = cityName;
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            start();
+        }
+
+        public void run() {
+            try {
+                while (!isStopped) {
+                    printTime();
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        private void printTime() throws InterruptedException {
+            //add your code here - добавь код тут
+        Thread.sleep(1000);
+        int time = hours * 3600 + minutes * 60 + seconds;
+         time++;
+        seconds = time % 60;
+        minutes = time / 60 % 60;
+        hours = time / 3600 % 24;
+            if (hours == 0 && minutes == 0 && seconds == 0) {
+                System.out.println(String.format("В г. %s сейчас полночь!", cityName));
+            } else {
+                System.out.println(String.format("В г. %s сейчас %d:%d:%d!", cityName, hours, minutes, seconds));
+            }
+        }
+    }
+}
